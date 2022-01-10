0e696e75655d2c55e571955d11a1164fde347b46
MaximGromak <maximgromak@gmail.com>
2022-01-10 20:57:09 +1000

    base modal window

---
+++ src/js/InteractionController.js
@@ -0,0 +1,32 @@
+export default class InteractionController {
+    constructor() {
+        this.setEditButtons();
+        this.setDeleteButtons();
+        this.setOpenTaskFunction();
+    }
+
+    static setEdit(btn) {
+        btn.addEventListener('click', (event) => {
+            event.preventDefault();
+
+
+        })
+    }
+
+    setEditButtons() {
+        const editBtns = Array.from(document.getElementsByClassName('edit'));
+        editBtns.forEach(btn => InteractionController.setEdit(btn));
+    }
+
+    setDeleteButtons() {
+
+    }
+
+    setOpenTaskFunction() {
+
+    }
+
+    setCloseTaskFunction() {
+
+    }
+}
--- src/js/TaskStatusHandler.js
+++ src/js/TaskStatusHandler.js
@@ -1,11 +1,16 @@
 export default class TaskStatusHandler {
     static parseStatus(statusId) {
         switch (statusId) {
-            case 0: return 'Новое';
-            case 1: return 'В работе';
-            case 2: return 'Завершено';
-            case 3: return 'Отклонено';
-            default: return 'не присвоен';
+            case 0:
+                return 'Новое';
+            case 1:
+                return 'В работе';
+            case 2:
+                return 'Завершено';
+            case 3:
+                return 'Отклонено';
+            default:
+                return 'не присвоен';
         }
     }
 }
