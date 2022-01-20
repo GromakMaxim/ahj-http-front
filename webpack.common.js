bcd853c85acfae902dc9b84f466617834ab75c32
MaximGromak <maximgromak@gmail.com>
2022-01-20 22:21:46 +1000

    upd settings

--- webpack.common.js
+++ webpack.common.js
@@ -5,8 +5,8 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     target: 'web',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: './',
     },
     resolve: {
         extensions: ['.js', '.jsx']
